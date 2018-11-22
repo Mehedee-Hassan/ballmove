da7307c6d01f7f4c966d7f9a4ff4c8894de78221
mehedee-hassan <razin.meha@gmail.com>
2018-11-22 16:14:41 +0600

    inital

---
+++ app/src/main/java/org/liquiddark/floatingclock/FloatingViewService.java
@@ -0,0 +1,301 @@
+package org.liquiddark.floatingclock;
+
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.PixelFormat;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.os.Build;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+import floatingclock.liquiddark.org.floatingclock.R;
+
+public class FloatingViewService extends Service implements SensorEventListener {
+
+
+    private WindowManager mWindowManager;
+    private View mFloatingView;
+    private BroadcastReceiver floatingBallReceiver;
+
+    public FloatingViewService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    WindowManager.LayoutParams params;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //Inflate the floating view layout we created
+        mFloatingView = LayoutInflater.from(this).inflate(R.layout.layout_floating_widget, null);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            params =
+                    new WindowManager.LayoutParams(
+                            WindowManager.LayoutParams.WRAP_CONTENT,
+                            WindowManager.LayoutParams.WRAP_CONTENT,
+                            WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+                            WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                            PixelFormat.TRANSLUCENT);
+
+        }
+
+        else {//Add the view to the window.
+
+            params =
+                    new WindowManager.LayoutParams(
+                            WindowManager.LayoutParams.WRAP_CONTENT,
+                            WindowManager.LayoutParams.WRAP_CONTENT,
+                            WindowManager.LayoutParams.TYPE_PHONE,
+                            WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                            PixelFormat.TRANSLUCENT);
+
+        }
+
+
+
+
+
+
+
+
+        //Specify the view position
+        params.gravity = Gravity.TOP | Gravity.LEFT;        //Initially view will be added to top-left corner
+        params.x = 0;
+        params.y = 100;
+
+        //Add the view to the window
+        mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        mWindowManager.addView(mFloatingView, params);
+
+        //The root element of the collapsed view layout
+        final View collapsedView = mFloatingView.findViewById(R.id.collapse_view);
+        //The root element of the expanded view layout
+        final View expandedView = mFloatingView.findViewById(R.id.expanded_container);
+
+
+        //Set the close button
+        ImageView closeButtonCollapsed = (ImageView) mFloatingView.findViewById(R.id.close_btn);
+        closeButtonCollapsed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //close the service and remove the from from the window
+                stopSelf();
+            }
+        });
+
+
+        //Set the view while floating view is expanded.
+        //Set the play button.
+        ImageView playButton = (ImageView) mFloatingView.findViewById(R.id.play_btn);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FloatingViewService.this, "Playing the song.", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        //Set the next button.
+        ImageView nextButton = (ImageView) mFloatingView.findViewById(R.id.next_btn);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FloatingViewService.this, "Playing next song.", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        //Set the pause button.
+        ImageView prevButton = (ImageView) mFloatingView.findViewById(R.id.prev_btn);
+        prevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FloatingViewService.this, "Playing previous song.", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        //Set the close button
+        ImageView closeButton = (ImageView) mFloatingView.findViewById(R.id.close_button);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                collapsedView.setVisibility(View.VISIBLE);
+                expandedView.setVisibility(View.GONE);
+            }
+        });
+
+
+        //Open the application on thi button click
+        ImageView openButton = (ImageView) mFloatingView.findViewById(R.id.open_button);
+        openButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Open the application  click.
+                Intent intent = new Intent(FloatingViewService.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+
+
+                //close the service and remove view from the view hierarchy
+                stopSelf();
+            }
+        });
+        mFloatingView.findViewById(R.id.root_container).setOnTouchListener(new View.OnTouchListener() {
+            private int initialX;
+            private int initialY;
+            private float initialTouchX;
+            private float initialTouchY;
+
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+
+
+                        //remember the initial position.
+                        initialX = params.x;
+                        initialY = params.y;
+
+
+                        //get the touch location
+                        initialTouchX = event.getRawX();
+                        initialTouchY = event.getRawY();
+                        return true;
+                    case MotionEvent.ACTION_MOVE:
+                        //Calculate the X and Y coordinates of the view.
+                        params.x = initialX + (int) (event.getRawX() - initialTouchX);
+                        params.y = initialY + (int) (event.getRawY() - initialTouchY);
+
+
+                        //Update the layout with new X & Y coordinate
+                        mWindowManager.updateViewLayout(mFloatingView, params);
+                        return true;
+
+                    case MotionEvent.ACTION_UP:
+                        int Xdiff = (int) (event.getRawX() - initialTouchX);
+                        int Ydiff = (int) (event.getRawY() - initialTouchY);
+
+
+                        //The check for Xdiff <10 && YDiff< 10 because sometime elements moves a little while clicking.
+                        //So that is click event.
+                        if (Xdiff < 10 && Ydiff < 10) {
+                            if (isViewCollapsed()) {
+                                //When user clicks on the image view of the collapsed layout,
+                                //visibility of the collapsed layout will be changed to "View.GONE"
+                                //and expanded view will become visible.
+                                collapsedView.setVisibility(View.GONE);
+                                expandedView.setVisibility(View.VISIBLE);
+                            }
+                        }
+                        return true;
+                }
+                return false;
+            }
+        });
+
+        breceiver();
+    }
+
+    private boolean isViewCollapsed() {
+        return mFloatingView == null || mFloatingView.findViewById(R.id.collapse_view).getVisibility() == View.VISIBLE;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mFloatingView != null) mWindowManager.removeView(mFloatingView);
+        this.unregisterReceiver(floatingBallReceiver);
+    }
+
+    long lastUpdate = 0;
+    float x,y,z, last_x , last_y , last_z;
+    int SHAKE_THRESHOLD = 300;
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getStringType() == Sensor.STRING_TYPE_ACCELEROMETER) {
+            long curTime = System.currentTimeMillis();
+            // only allow one update every 100ms.
+            if ((curTime - lastUpdate) > 100) {
+                long diffTime = (curTime - lastUpdate);
+                lastUpdate = curTime;
+
+                x = event.values[0];
+                y = event.values[1];
+                z = event.values[2 ];
+
+                float speed = Math.abs(x+y+z - last_x - last_y - last_z) / diffTime * 10000;
+
+                if (speed > SHAKE_THRESHOLD) {
+                    Log.d("sensor", "shake detected w/ speed: " + speed);
+                    Toast.makeText(this, "shake detected w/ speed:SERVICE " + speed, Toast.LENGTH_SHORT).show();
+
+                    params.x = 0;
+                    params.y = 400;
+
+
+                    //Update the layout with new X & Y coordinate
+                    mWindowManager.updateViewLayout(mFloatingView, params);
+
+
+
+                }
+                last_x = x;
+                last_y = y;
+                last_z = z;
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    Random random = new Random();
+    int nextFlag = 100;
+    private void breceiver(){
+        floatingBallReceiver = new BroadcastReceiver() { // init your Receiver
+
+            @Override
+            public void onReceive(Context context, Intent intent) {
+
+                Log.d("Apps", "I'm in the onReceive");
+                
+                for(int i = 0 ; i <nextFlag ; i++ ) {
+                    params.x = (int) (intent.getFloatExtra("X", 0f) + i)* 10;
+                    params.y = (int) (intent.getFloatExtra("Y", 0f) + i)* 10;
+                }
+               //Update the layout with new X & Y coordinate
+                mWindowManager.updateViewLayout(mFloatingView, params);
+
+            }
+        };
+        // register your Receiver after initialization
+        this.registerReceiver(floatingBallReceiver,
+                new IntentFilter("org.liquiddark.MOVING"));
+    }
+}
---
+++ app/src/main/java/org/liquiddark/floatingclock/MainActivity.java
@@ -0,0 +1,133 @@
+package org.liquiddark.floatingclock;
+
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorListener;
+import android.hardware.SensorManager;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import floatingclock.liquiddark.org.floatingclock.R;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+    private static final int CODE_DRAW_OVER_OTHER_APP_PERMISSION = 2084;
+    SensorManager sensorManager;
+    private Sensor accSensor;
+    private Sensor magnetSensor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        accSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magnetSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+        sensorManager.registerListener(this, accSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, magnetSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+
+
+
+        //Check if the application has draw over other apps permission or not?
+        //This permission is by default available for API<23. But for API > 23
+        //you have to ask for the permission in runtime.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+
+
+            //If the draw over permission is not available open the settings screen
+            //to grant the permission.
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, CODE_DRAW_OVER_OTHER_APP_PERMISSION);
+        } else {
+            initializeView();
+        }
+    }
+
+    /**
+     * Set and initialize the view elements.
+     */
+    private void initializeView() {
+        findViewById(R.id.notify_me).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this, FloatingViewService.class));
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CODE_DRAW_OVER_OTHER_APP_PERMISSION) {
+            //Check if the permission is granted or not.
+            if (resultCode == RESULT_OK) {
+                initializeView();
+            } else { //Permission is not available
+                Toast.makeText(this,
+                        "Draw over other app permission not available. Closing the application",
+                        Toast.LENGTH_SHORT).show();
+
+                finish();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+
+    long lastUpdate = 0;
+    float x,y,z, last_x , last_y , last_z;
+    int SHAKE_THRESHOLD = 400;
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getStringType() == Sensor.STRING_TYPE_ACCELEROMETER) {
+            long curTime = System.currentTimeMillis();
+            // only allow one update every 100ms.
+            if ((curTime - lastUpdate) > 10) {
+                long diffTime = (curTime - lastUpdate);
+                lastUpdate = curTime;
+
+                x = event.values[0];
+                y = event.values[1];
+                z = event.values[2];
+
+                float speed = Math.abs(x+y+z - last_x - last_y - last_z) / diffTime * 10000;
+
+                if (speed > SHAKE_THRESHOLD) {
+                    Log.d("sensor", "shake detected w/ speed: " + speed);
+                    Toast.makeText(this, "shake detected w/ speed: ACTIVITY " + speed, Toast.LENGTH_SHORT).show();
+
+                    startService(new Intent(MainActivity.this, FloatingViewService.class));
+                    this.finish();
+
+                    Intent intent = new Intent("org.liquiddark.MOVING");
+                    intent.putExtra("X",x);
+                    intent.putExtra("Y",y);
+                    sendBroadcast(intent);
+                }
+                last_x = x;
+                last_y = y;
+                last_z = z;
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
+
